d2a920b6b35b36cea60cf633156da93bcd9de7b7
SabinaAbbasova <50209542+sabina929@users.noreply.github.com>
2019-06-13 17:06:07 +0400

    Update Personal_portfolio.js

--- Personal_portfolio.js
+++ Personal_portfolio.js
@@ -1,9 +1,11 @@
+ console.clear();
  window.addEventListener("mousemove", animate);
 
  function animate(event) {
      var x = event.clientX;
      var y = event.clientY;
      var randomNum, randomColor, size, el;
+     //  randomColor = "rgb(" + Math.random() * 255 + "," + Math.random() * 255 + "," + Math.random() * 255 + "," + Math.random() + ")";
      randomColorAlpha = "rgb(" + Math.random() * 255 + "," + Math.random() * 255 + "," + Math.random() * 255 + ", 0.4)";
      randomColor = "#" + Math.random().toString(16).slice(-6);;
 
@@ -20,16 +22,65 @@
          "deg) scale(" + Math.random() * 2 + ") translate(" + randomNum + "px)");
 
 
+     if (document.getElementsByClassName("shape")) {
+         var frames, frameCount, i, interval;
+         frames = document.getElementsByClassName("shape");
+         frameCount = frames.length;
+         i = 0;
+         interval = setInterval(function () {
+             if (frameCount > 8) {
+                 document.getElementById("load").removeChild(frames[i]);
+                 clearInterval(interval);
+                 console.clear();
+                 //  for (i = 9; i >= 0; i--) {
+                 //      frames[i].style.opacity = (i / 0.9) - i;
+                 //      frames[i].style.opacity = (frames[i].style.opacity).subsrt(0, 3);
+                 //  }
+             }
+         })
+     }
+ }
+
+
+
+ /*
+ window.addEventListener("mousemove", animate);
+
+ function animate(event) {
+     var x = event.clientX;
+     var y = event.clientY;
+     var size, el;
+     // randomColor = "rgb(" + Math.random() * 255 + "," + Math.random() * 255 + "," + Math.random() * 255 + "," + Math.random() + ")";
+     // randomColorAlpha = "rgb(" + Math.random() * 255 + "," + Math.random() * 255 + "," + Math.random() * 255 + ", 0.4)";
+     // randomColor = "#" + Math.random().toString(16).slice(-6);;
+
+     size = Math.random() * 20;
+     // randomNum = Math.random() * 30;
+     el = document.createElement("div");
+
+     document.body.appendChild(el);
+     el.classList.add("shape");
+     el.setAttribute("style", "z-index:99999;position:absolute;background:rgb(70, 165, 158);width:" + size +
+         "px;height:" + size + "px;left:" +
+         x + "px;top:" +
+         y + "px;border-radius:" + Math.random() * 100 + "%");
+     // box - shadow: 0 0 " + size + "px rgba(255, 165, 0, 0.5)
+
      if (document.querySelectorAll(".shape")) {
          var frames, frameCount, i, interval;
          frames = document.querySelectorAll(".shape");
          frameCount = frames.length;
          i = 0;
          interval = setInterval(function () {
-             if (frameCount > 5) {
+             if (frameCount > 8) {
                  document.getElementById("load").removeChild(frames[i]);
                  clearInterval(interval);
+                 //  for (i = 9; i >= 0; i--) {
+                 //      frames[i].style.opacity = (i / 0.9) - i;
+                 //      frames[i].style.opacity = (frames[i].style.opacity).subsrt(0, 3);
+                 //  }
              }
          })
      }
  }
+ */
